a5b49ae992e380708d3b7e8693ca37d136b0314d
firos-67 <firoschalissery@gmail.com>
2022-12-01 11:33:51 +0530

    added

---
+++ movieproject/movieprojectapp/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from movieproject import settings
+from django.conf.urls.static import static
+from . import views
+app_name='movieprojectapp'
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('movie/<int:movie_id>/',views.details,name='details'),
+    path('add/',views.add,name='add'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('delete/<int:id>',views.delete,name='delete'),
+]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL,
+                        document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
+
+
